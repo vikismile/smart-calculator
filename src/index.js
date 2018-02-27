0377d4308aef583ef02da17f9fc04170f1838ff6
viktoria <vikaorabey@mail.ru>
2018-02-27 19:00:53 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,26 +1,62 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.ar = [initialValue];
   }
 
   add(number) {
-    // your implementation
+    this.ar.push('+', number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.ar.push('-', number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.ar.push('*', number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.ar.push('/', number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.ar.push('^', number);
+    return this;
+  }
+  
+  toString(){
+    console.log(this.ar);
+      for(var j=0; j<this.ar.length; j++){
+        if(this.ar[j]=='^') {
+          this.ar.splice(j - 1, 3 , Math.pow(this.ar[j-1],this.ar[j+1]));
+          j = 0;
+        }}
+        for(var j=0; j<this.ar.length; j++){
+        if(this.ar[j]=='*'){
+          this.ar.splice(j - 1, 3, this.ar[j+1]*this.ar[j-1]);
+          j = 0;
+        }}
+        for(var j=0; j<this.ar.length; j++){
+        if(this.ar[j]=='/'){
+          this.ar.splice(j - 1, 3, this.ar[j-1]/this.ar[j+1]);
+          j = 0;
+        }}
+        for(var j=0; j<this.ar.length; j++){
+        if(this.ar[j]=='-'){
+          this.ar.splice(j - 1, 3, this.ar[j-1]-this.ar[j+1]);
+          j = 0;
+        }}
+        for(var j=0; j<this.ar.length; j++){
+        if(this.ar[j]=='+'){
+          this.ar.splice(j - 1, 3, this.ar[j+1]+this.ar[j-1]);
+          j = 0;
+        }}
+      
+    return this.ar[0];
   }
 }
 
